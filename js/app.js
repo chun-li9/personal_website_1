4802068f5df45b5b5e5c1a86f136f80abc3bbb09
Chun160712 <chun160712@gmail.com>
2021-12-22 15:05:24 -0500

    first commit: half done navigation bar

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const switcher = document.querySelector('.btn'); // get the button reference
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark Mode";
+    }
+    else {
+        this.textContent = "Light Mode";
+    }
+
+    console.log('current class name: ' + className);
+});
