2e9673094a98f886625b353e37ca01d7c8a0197c
Chun160712 <chun160712@gmail.com>
2021-12-26 15:09:28 -0500

    implement contact section

--- js/app.js
+++ js/app.js
@@ -7,11 +7,36 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
+
     // toggle menu/navbar
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    })
+    });
+
+    // owl carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 500,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
 });
 
 
