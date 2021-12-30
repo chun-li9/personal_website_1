ccce0efc5837b7d233cccd9114e2212184f086a8
Chun160712 <chun160712@gmail.com>
2021-12-30 11:47:54 -0500

    add loading page and half-done home page animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 
+// Loading page progress bar fade out
+var removeLoadingBar = function(){
+    $(".progress").fadeOut(500).empty();  
+}
+setTimeout(removeLoadingBar, 4000); 
+
+// entire loading page fade out
+function removeLoader(){
+    setTimeout(function(){
+        window.scrollTo({top:0, behaviour:'auto'});
+    },0);
+    $( ".loader-wrapper" ).fadeOut(1000, function() {
+        $( ".loader-wrapper" ).remove(); 
+    });  
+}
+  
+setTimeout(removeLoader, 6000);
+
+
+// main page activity
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -26,7 +46,7 @@ $(document).ready(function(){
 
     // typing animation
     var typed = new Typed(".typing", {
-        strings: ["Hello, my name is", "Bonjour, je m'appelle", "你好，我是"],
+        strings: ["Hello, my name is", "你好，我是"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
@@ -55,7 +75,46 @@ $(document).ready(function(){
         }
     });
 
+    // cursor trail
+    cursorX = 0;
+    cursorY = 0;
+
+    let colors = ['rgb(128,128,128)', 'rgb(225,225,225)', 'rgb(250, 242, 225)', 'rgb(158, 123, 93)', 'rgb(185, 120, 94)', 'rgb(216, 172, 151)'];
+  
+    function getRndInt(min, max) { // both ends included
+        return Math.round(Math.random() * (max - min + 1)) + min;
+    }
+    
+    $(window).mousemove(function(e) {
+      cursorX = e.pageX;
+      cursorY = e.pageY;
+    });
+    
+    $(window).mouseleave(function() {
+      cursorX = -1;
+      cursorY = -1;
+    });
+    
+    setInterval(function(){
+      if(cursorX > 0 && cursorY > 0){
+        var range = 5;
+        var sizeInt = getRndInt(5,15);
+        var color_index = getRndInt(0,5);
+  
+        var style = "height:" + sizeInt + "px; " 
+        + "width:" + sizeInt + "px; "
+        + "left:" + getRndInt(cursorX-range-sizeInt, cursorX+range) + "px;"
+        + "top:" + getRndInt(cursorY-range-sizeInt, cursorY+range) + "px;"
+        + "background:" + colors[color_index] + ";"
+        + "opacity:" + Math.random();
+        
+        $("<div class='trail' style='" + style + "'></div>").appendTo('#entire-body'); 
+      }
+    }, 25);
+
 });
+
+
 
 
 /*
@@ -76,3 +135,5 @@ switcher.addEventListener('click', function() {
 });
 
 */
+
+
